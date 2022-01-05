966747f1a51df584668cfbe19aeb2f1645df6d51
Neel <neelshah6892@gmail.com>
2022-01-05 09:53:40 +0530

    All Files Upload

---
+++ StringSplit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ticker = "BANKNIFTY28NOV2140000CE.NFO";
+
+            
+
+        }
+    }
+}
