a4e0230c3cc29dfcb00437e02bfd3f482dd47d17
Neel <neelshah6892@gmail.com>
2022-01-27 10:31:14 +0530

    New Files Upload

---
+++ Project1/Project1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using sysreadern;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Project1
+{
+    class Class1
+    {
+        [DllImport("sysreadern.dll")]
+        //public static extern 
+        public static extern int ProcessMBR(char* streamBuffer, char* unCompbuffer, int filterTrCode, TKTABLE* tktable, long filterTkn, short saveFlg);
+
+        //int z = ProcessMBR();
+        Debug.WriteLine("Send to debug output.");
+    }
+
+}
