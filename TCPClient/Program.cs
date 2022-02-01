f294fa0a4b1894c1829e4ca99b290639bfb06d5c
Neel <neelshah6892@gmail.com>
2022-02-01 11:22:53 +0530

    Implemented TCP

---
+++ TCPClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            TcpClient clientSocket = new TcpClient();
+            clientSocket.Connect("localhost", 5000);
+            Console.WriteLine(clientSocket.Connected);
+
+            var stream = clientSocket.GetStream();
+
+            clientSocket.SendTimeout = 30;
+            clientSocket.ReceiveTimeout = 30;
+
+            var txt = Encoding.ASCII.GetBytes("\ntest");
+
+            stream.Write(txt, 0, txt.Length);
+
+            byte[] bt = new byte[1024];
+
+            int bytesread = clientSocket.Client.Receive(bt);
+            string result = Encoding.ASCII.GetString(bt, 0, bytesread);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TcpListener listener = new TcpListener(IPAddress.Broadcast,)
+            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+
+            var client = listener.AcceptTcpClient();
+
+            NetworkStream networkStream = client.GetStream();
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.AutoFlush = true;
+
+            while (true)
+            {
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                int bytesRead = networkStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                string datareceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                if (bytesRead != 0)
+                {
+                    Console.WriteLine("Received Request: \n" + datareceived);
+                    string response = "Worked";
+                    Console.WriteLine(response);
+                    writer.WriteLine(response + "\r\n");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
