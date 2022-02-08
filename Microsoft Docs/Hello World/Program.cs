b25624a6a3a3dc490435a6440d30a4782a90f262
Neel <neelshah6892@gmail.com>
2022-02-08 11:19:40 +0530

    First Steps

---
+++ Microsoft Docs/Hello World/Program.cs
@@ -0,0 +1,7 @@
+﻿//Write your first code with c#
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("This is the First Line.");
+Console.WriteLine("This is the second line.");
+
+//Store and retrieve data using literals
