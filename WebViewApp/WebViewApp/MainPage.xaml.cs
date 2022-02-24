2744ba585b7c495f822e82c143f0f0c5f3e42f37
Neel <neelshah6892@gmail.com>
2022-02-24 14:23:06 +0530

    All File Upload with Changes

---
+++ WebViewApp/WebViewApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebViewApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Webview.Source = "https://novasolutions.co.in";
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await progress.ProgressTo(0.9, 900, Easing.SpringIn);
+        }
+
+        protected void OnNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            progress.IsVisible = true;
+        }
+
+        protected void OnNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            progress.IsVisible = false;
+        }
+
+    }
+}
