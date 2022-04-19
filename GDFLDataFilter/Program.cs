6aadafb46649656763c59992338397732c909b9d
Neel <neelshah6892@gmail.com>
2022-04-19 13:20:44 +0530

    All File Updates

---
+++ GDFLDataFilter/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+Application excel = new Application();
+Workbook wb = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\GFDLNFO_BACKADJUSTED_18042022.csv");
+Worksheet ws = wb.Worksheets["GFDLNFO_BACKADJUSTED_18042022"];
+excel.Visible = true;
+excel.DisplayAlerts = false;
+
+Thread.Sleep(10000);
+
+excel.Quit();
