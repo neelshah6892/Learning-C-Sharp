8cd62f81e87196a22897d1344ffae139efadac6b
Neel <neelshah6892@gmail.com>
2022-04-27 13:17:39 +0530

    All File Updates

--- GDFLDataFilter/Program.cs
+++ GDFLDataFilter/Program.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.Office.Interop.Excel;
+using System.Xml.Linq;
 
 Application excel = new Application();
-Workbook wb = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\GFDLNFO_BACKADJUSTED_18042022.csv");
+/*Workbook wb = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\GFDLNFO_BACKADJUSTED_18042022.csv");
 Worksheet ws = wb.Worksheets["GFDLNFO_BACKADJUSTED_18042022"];
 excel.Visible = true;
 excel.DisplayAlerts = false;
 
-Thread.Sleep(10000);
+Thread.Sleep(10000);*/
+
+LoadOptions loadOptions = new LoadOptions();
+//loadOptions.Password = password;
+
+Workbook workbook = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\IV file.xlsx", loadOptions);
+workbook.Unprotect();
+// or workbook.Settings.Password = "";
+workbook.Save(filePath);
 
 excel.Quit();
