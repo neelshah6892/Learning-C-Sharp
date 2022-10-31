ec4d4e2cfb20ccf46b74ca949145049b15966f91
Neel <neelshah6892@gmail.com>
2022-10-31 15:21:04 +0530

    All File Updates

---
+++ SampleToDo/Models/UserRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SampleToDo.Models
+{
+    public class UserRoles
+    {
+        
+        public const string Admin = "Admin";
+        public const string User = "User";
+        
+    }
+}
