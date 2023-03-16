b7ab1bed1cd64c7fa68c6991698056600b45aadb
Neel <neelshah6892@gmail.com>
2023-03-16 22:05:21 +0530

    All File Updates

---
+++ WriteExcel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WriteExcel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWb = excelApp.Workbooks.Add("");
+            Excel._Worksheet excelWS = excelWb.ActiveSheet;
+            excelWS.Cells[1, 1] = "First Name";
+            excelWS.Cells[1, 2] = "Last Name";
+            excelWS.Cells[2, 1] = "Neel";
+            excelWS.Cells[2, 2] = "Shah";
+            excelWb.SaveCopyAs(@"C:\Users\dhwan\Desktop\Demo.xlsx");
+            excelWb.Close();
+            excelApp.Quit();
+        }
+    }
+}
