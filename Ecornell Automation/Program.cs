5796c98da16509bd51558b9b9d4ef909f252ed08
Neel <neelshah6892@gmail.com>
2023-03-27 20:53:28 +0530

    All File Updates

---
+++ Ecornell Automation/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium_Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:\\Users\\dhwan\\Desktop\\chromedriver_win32");     
+            driver.Url = "https://ondemand.ecornell.com/";
+
+            var name = "New Folder";
+            string root = @"C:\Users\dhwan\Desktop\"+name;
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+
+        }
+    }
+}
