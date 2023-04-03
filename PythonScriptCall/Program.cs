01f0215f1d623bdc48ae8428c0770a583160c40d
Neel <neelshah6892@gmail.com>
2023-04-03 13:47:07 +0530

    All file Updates

--- PythonScriptCall/Program.cs
+++ PythonScriptCall/Program.cs
@@ -31,8 +31,8 @@ namespace PythonScriptCall
             //DateTime daybeforeyesterday = DateTime.Now.AddDays(-3);
 
             //1 day holiday(tuesday)
-            //DateTime yesterday = DateTime.Now.AddDays(-2);
-            //DateTime daybeforeyesterday = DateTime.Now.AddDays(-5);
+            DateTime yesterday = DateTime.Now.AddDays(-2);
+            DateTime daybeforeyesterday = DateTime.Now.AddDays(-5);
 
             //DateTime yesterday = DateTime.Now.AddDays(-1);
             //DateTime daybeforeyesterday = DateTime.Now.AddDays(-3);
@@ -45,8 +45,8 @@ namespace PythonScriptCall
             //DateTime yesterday = DateTime.Now.AddDays(-1);
             //DateTime daybeforeyesterday = DateTime.Now.AddDays(-5);
 
-            DateTime yesterday = DateTime.Now.AddDays(-3);
-            DateTime daybeforeyesterday = DateTime.Now.AddDays(-5);
+            //DateTime yesterday = DateTime.Now.AddDays(-3);
+            //DateTime daybeforeyesterday = DateTime.Now.AddDays(-5);
 
             //DateTime yesterday = DateTime.Now.AddDays(-2);
             //DateTime daybeforeyesterday = DateTime.Now.AddDays(-3);
