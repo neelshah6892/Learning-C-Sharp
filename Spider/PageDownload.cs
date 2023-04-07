07a34abbb4bf28c413eb9ced82224010a0241c90
Neel <neelshah6892@gmail.com>
2023-04-07 17:55:28 +0530

    All File Updates

---
+++ Spider/PageDownload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+
+namespace Spider
+{
+    internal class PageDownload
+    {
+        string pageSource = driver.PageSource;
+        Console.WriteLine(pageSource);
+        var path2 = "data.html";
+        File.WriteAllText(path2, pageSource);
+    }
+}
--- Spider/Program.cs
+++ Spider/Program.cs
@@ -14,11 +14,11 @@ namespace Spider
             var options = new ChromeOptions();
 
             //Laptop
-            options.AddArgument("user-data-dir=C:\\Users\\dhwan\\AppData\\Local\\Google\\Chrome\\User Data");
+            //options.AddArgument("user-data-dir=C:\\Users\\dhwan\\AppData\\Local\\Google\\Chrome\\User Data");
 
             //dhwani System
-            //options.AddArgument("user-data-dir=C:\\Users\\dhs71\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 5");
-            //options.AddArgument("--profile_ddirectory=Profile 5");
+            options.AddArgument("user-data-dir=C:\\Users\\dhs71\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 5");
+            options.AddArgument("--profile_ddirectory=Profile 5");
 
             //Main laptop
             //options.AddArgument("--profile_directory=Arjun");
@@ -30,39 +30,38 @@ namespace Spider
             ChromeDriver driver = new ChromeDriver("./", options);
 
             driver.Manage().Window.Maximize();
-            driver.Url = "https://ondemand.ecornell.com/";
+            //driver.Url = "https://ondemand.ecornell.com/";
+            driver.Url = "https://ondemand.ecornell.com/lesson.do?lessonCode=ILR562OD3";
             
+
+            Thread.Sleep(60000);
+            /*driver.FindElement(By.XPath("//*[@id=\"nav-tray-portal\"]/span/span/div/div/div/span/button")).Click();
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//*[@id=\"step-1\"]/div[3]/button")).Click();*/
+
+            
+            driver.FindElement(By.XPath("//*[@id=\"wiki_page_show\"]/div[3]/table/tbody/tr[5]/td[2]/a")).Click();
+            Thread.Sleep(10000);
+            /*driver.FindElement(By.XPath("//*[@id=\"nav-tray-portal\"]/span/span/div/div/div/span/button")).Click();
+            
+            Thread.Sleep(2000);*/
+            driver.FindElement(By.XPath("//*[@id=\"module_navigation_target\"]/div/div[2]/div/span/a")).Click();
+
+            driver.FindElement(By.XPath("//*[@id=\"term_skill\"]/ul/li[1]/div/label")).Click();
             
             string pageSource = driver.PageSource;
             Console.WriteLine(pageSource);
-            var path = "dtat.html";
-            File.WriteAllText(path, pageSource);
-            Console.Read();
-
-            /*Thread.Sleep(5000);
-            driver.FindElement(By.XPath("//*[@id=\"username\"]")).Click();
-            driver.FindElement(By.XPath("//*[@id=\"username\"]")).SendKeys(Keys.ArrowDown);
-            driver.FindElement(By.XPath("//*[@id=\"username\"]")).SendKeys(Keys.Enter);
-            driver.FindElement(By.XPath("//*[@id=\"fm1\"]/div[2]/input")).Click();
-            //driver.FindElement(By.XPath("//*[@id=\"fm1\"]/div[3]/input")).Click();
-            /hread.Sleep(10000);*/
-            //Console.ReadKey();
-            Thread.Sleep(100000);
-            //driver.FindElement(By.XPath("//*[@id=\"term_skill\"]/a[2]")).Click();
-            driver.FindElement(By.XPath("//*[@id=\"term_skill\"]/ul/li[1]/div/label")).Click();
-            
-            string pageSource2 = driver.PageSource;
-            Console.WriteLine(pageSource);
             var path2 = "data.html";
-            File.WriteAllText(path2, pageSource2);
-            Console.Read();
+            File.WriteAllText(path2, pageSource);
+            //Console.Read();
             
-            Thread.Sleep(10000);
+            Thread.Sleep(20000);
             //driver.SwitchTo().NewWindow(WindowType.Tab);
-            driver.FindElement(By.ClassName("uk-card-body uk-link-reset uk-position-relative ec-details-link")).Click();
-            string name = driver.FindElement(By.ClassName("uk-card-body uk-link-reset uk-position-relative ec-details-link")).ToString();
-            Console.WriteLine(name);
-            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//*[@id=\"ecCards\"]/div[2]/div[1]/div[1]/a")).Click();
+            Thread.Sleep(10000);
+            //string coureLink = driver.FindElement(By.XPath("//*[@id=\"ecCards\"]/div[2]/div[2]/div/div/ul[1]/li[6]/a")).ToString();
+            //driver.FindElement(By.ClassName("uk-card-body uk-link-reset uk-position-relative ec-details-link")).Click();
+            Thread.Sleep(100000);
 
             driver.FindElement(By.ClassName("moving-btn")).Click();
             Thread.Sleep(5000);
