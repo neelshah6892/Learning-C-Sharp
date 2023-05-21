7c35538ed611a7cf183dda97a31bc9d86dae8bc7
Neel <neelshah6892@gmail.com>
2023-05-21 21:31:48 +0530

    All File Updates

---
+++ nickchapsas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+var random = new Random(420);
+var items = Enumerable.Range(0, 100).Select(x => random.Next()).ToList();
+
+Span<int> listAsSpan = CollectionsMarshal.AsSpan(items);
+ref var searchSpace = ref MemoryMarshal.GetReference(listAsSpan);
+for (var i = 0; i<listAsSpan.Length; i++){
+    var item = Unsafe.Add(ref searchSpace, i);
+    Console.WriteLine(item);
+}
+
+record Wrapper(int Number, string txt);
