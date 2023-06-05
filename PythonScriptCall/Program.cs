281400d7dab4b63a0e83290f107f94484c2ca14e
Neel <neelshah6892@gmail.com>
2023-06-05 13:56:14 +0530

    All File Updates

--- PythonScriptCall/Program.cs
+++ PythonScriptCall/Program.cs
@@ -19,12 +19,12 @@ namespace PythonScriptCall
             //DateTime daybeforeyesterday = DateTime.Now.AddDays(-2);
 
             //Monday or 2 day holiday
-            DateTime yesterday = DateTime.Now.AddDays(-3);
-            DateTime daybeforeyesterday = DateTime.Now.AddDays(-4);
+            //DateTime yesterday = DateTime.Now.AddDays(-3);
+            //DateTime daybeforeyesterday = DateTime.Now.AddDays(-4);
 
             //Tuesday
-            //DateTime yesterday = DateTime.Now.AddDays(-1);
-            //DateTime daybeforeyesterday = DateTime.Now.AddDays(-4);
+            DateTime yesterday = DateTime.Now.AddDays(-1);
+            DateTime daybeforeyesterday = DateTime.Now.AddDays(-4);
 
             //1 day holiday
             //DateTime yesterday = DateTime.Now.AddDays(-2);
@@ -178,7 +178,7 @@ namespace PythonScriptCall
             File.Delete("C:\\Users\\Administrator\\Desktop\\" + yesterday.ToString("ddMMyyyy") + ".xlsx");
             Thread.Sleep(2000);
 
-            /*Console.WriteLine("Macro Process");
+            Console.WriteLine("Macro Process");
             Application excel = new Application();
             Workbook wbook = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\Vol 2021.xlsb");
             excel.Visible = true;
@@ -193,7 +193,7 @@ namespace PythonScriptCall
             foreach (Process processes in Process.GetProcessesByName("Excel"))
             {
                 processes.Kill();
-            }*/
+            }
             
             //File.Delete("C:\\Users\\Administrator\\Desktop\\" + yesterday.ToString("ddMMyyyy") + ".xlsx");
             Console.WriteLine("Completed");
