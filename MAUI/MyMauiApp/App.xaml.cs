3fc7bfadeef2809043f3d333c0c9825daa3b91af
Neel <neelshah6892@gmail.com>
2023-09-10 20:24:00 +0530

    All File Updates

---
+++ MAUI/MyMauiApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MyMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+}
