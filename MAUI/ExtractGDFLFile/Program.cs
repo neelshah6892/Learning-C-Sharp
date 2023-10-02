331200c587f549b9959387adcfb911a32f64e678
Neel <neelshah6892@gmail.com>
2023-10-02 21:37:47 +0530

    All File Updates

---
+++ MAUI/ExtractGDFLFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Compression;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DateTime today = DateTime.Now;
+        Console.WriteLine(today.ToString("ddMMyyy"));
+        /*string startPath = @".\start";
+        string zipPath = @".\result.zip";
+        string extractPath = @".\extract";
+
+        ZipFile.CreateFromDirectory(startPath, zipPath);
+
+        ZipFile.ExtractToDirectory(zipPath, extractPath);*/
+    }
+}
