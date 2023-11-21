15c99305a815bdf21d611d58132fab8db954f3e4
Neel <neelshah6892@gmail.com>
2023-11-21 22:38:14 +0530

    All File Updates

---
+++ Test/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.IO.Compression;
+
+string dirName = @"C:\Users\dhwan\Dowmloads\20112023";
+string zipName = @"C:\Users\dhwan\Downloads\20112023.zip";
+
+ZipFile.ExtractToDirectory(zipName, dirName, true);
