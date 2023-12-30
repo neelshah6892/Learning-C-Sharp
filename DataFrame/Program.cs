4afb39d6772f5fcb84ddc48755c4c022f636e7af
Neel <neelshah6892@gmail.com>
2023-12-30 12:54:20 +0530

    All File Updates

---
+++ DataFrame/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Data.Analysis;
+
+var fname = Path.GetFullPath("products.csv");
+var df = DataFrame.LoadCsv(fname);
+
+Console.WriteLine(df.Info());
+Console.WriteLine(df.Description());
