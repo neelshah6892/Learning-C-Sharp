5c552b13574c4586aa5ac6fec975287365336a7d
Neel <neelshah6892@gmail.com>
2024-01-31 07:47:28 +0530

    All File Updates

---
+++ AES256/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace AES256
+{
+    class Program
+    {
+        private static string getString(byte[] b)
+        {
+            return Encoding.UTF8.GetString(b);
+        }
+        static void Main(string[] args)
+        {
+            byte[] data = Encoding.UTF8.GetBytes("Kifs@12345678");
+            byte[] a = Convert.FromBase64String("fogoZNg4jLgROSA7gnNt3gVxUwKxZGUG94swhWSVZXE=");
+            Console.WriteLine("Key : {0}", getString(a)); byte[] enc = Encrypt(data, a); string result = Convert.ToBase64String(enc); Console.WriteLine("Encrypted text", result); byte[] dec = Decrypt(enc, a);
+            Console.WriteLine("Encrypted : {0}", getString(enc)); Console.WriteLine("Decrypted : {0}", getString(dec));
+            // Console.ReadKey();
+        }
+        public static byte[] Encrypt(byte[] data, byte[] key)
+        {
+            using (RijndaelManaged csp = new RijndaelManaged())
+            {
+                csp.KeySize = 256; csp.BlockSize = 128; csp.Key = key; csp.Padding = PaddingMode.PKCS7; csp.Mode = CipherMode.ECB;
+                CryptoTransform encrypter = csp.CreateEncryptor(); return encrypter.TransformFinalBlock(data, 0, data.Length);
+            }
+        }
+        private static byte[] Decrypt(byte[] data, byte[] key)
+        {
+            using (RijndaelManaged csp = new RijndaelManaged())
+            {
+                csp.KeySize = 256; csp.BlockSize = 128; csp.Key = key; csp.Padding = PaddingMode.PKCS7; csp.Mode = CipherMode.ECB;
+                CryptoTransform decrypter = csp.CreateDecryptor(); return decrypter.TransformFinalBlock(data, 0, data.Length);
+            }
+        }
+    }
+}
