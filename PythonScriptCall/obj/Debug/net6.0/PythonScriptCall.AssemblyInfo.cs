c6fe752049cb215fb4b8d7bd2c435c0a7543164f
Neel <neelshah6892@gmail.com>
2024-03-04 22:55:14 +0530

    All File Updates

--- PythonScriptCall/obj/Debug/net6.0/PythonScriptCall.AssemblyInfo.cs
+++ PythonScriptCall/obj/Debug/net6.0/PythonScriptCall.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PythonScriptCall")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef791f973262872edb5aa62b737bbdde1da3e375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19e22240a49758eb172619ce691440cdcd043f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PythonScriptCall")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PythonScriptCall")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
