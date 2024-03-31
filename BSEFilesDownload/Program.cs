f300a28d1d0a8cea41599c38c9b1cf4a921d0b27
Neel <neelshah6892@gmail.com>
2024-03-31 20:18:16 +0530

    All File Updates

---
+++ BSEFilesDownload/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System.Threading;
+
+namespace EdgeDriverSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new EdgeDriver();
+            try
+            {
+                driver.Url = "https://www.bseindia.com/members/index.aspx?expandable=6";
+
+                Thread.Sleep(5000);
+
+                 var element = driver.FindElement(By.XPath("//*[@id=\"divID\"]/div[5]/div[2]/p[1]/strong/span[2]/span/a"));
+
+                element.Click();
+
+                Thread.Sleep(5000);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
