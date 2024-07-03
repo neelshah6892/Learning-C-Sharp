8eaed4d1f5ba5de948af11562c599ca07a2e16be
Neel <neelshah6892@gmail.com>
2024-07-03 15:57:59 +0530

    All file Updates

---
+++ DeleteAllZip/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FileOperationsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change this path to the directory you want to read
+            string path = "G:\\";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            Console.WriteLine("File Name                       Size        Creation Date and Time");
+            Console.WriteLine("=================================================================");
+            foreach (FileInfo flInfo in dir.GetFiles())
+            {
+                String name = flInfo.Name;
+                long size = flInfo.Length;
+                DateTime creationTime = flInfo.CreationTime;
+                Console.WriteLine("{0, -30:g} {1,-12:N0} {2} ", name, size, creationTime);
+            }
+            foreach (FileInfo file in dir.GetFiles("*.exe"))
+            {
+                file.Delete();
+            }
+            Console.WriteLine("=================================================================");
+            foreach (FileInfo flInfo in dir.GetFiles())
+            {
+                String name = flInfo.Name;
+                long size = flInfo.Length;
+                DateTime creationTime = flInfo.CreationTime;
+                Console.WriteLine("{0, -30:g} {1,-12:N0} {2} ", name, size, creationTime);
+            }
+            Console.ReadLine();
+        }
+    }
+}
