128dce23c0c5bbb1c906d5f31417b915764958ea
Neel <neelshah6892@gmail.com>
2024-07-26 12:22:25 +0530

    u

--- ExtractZipFromFolder/Program.cs
+++ ExtractZipFromFolder/Program.cs
@@ -1,4 +1,40 @@
-﻿using System.IO.Compression;
+﻿using System;
+using System.IO.Compression;
+
+namespace unzip_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipFilePath = @"E:\BhavCopy_NSE_FO_0_0_0_20240725_F_0000.csv.zip";
+            string extractionPath = @"E:\";
+            ZipFile.ExtractToDirectory(zipFilePath, extractionPath);
+            Console.WriteLine("Extracted Successfully");
+
+            string zipFilePath1 = @"E:\25072024.zip";
+            string extractionPath1 = @"E:\";
+            ZipFile.ExtractToDirectory(zipFilePath1, extractionPath1);
+            Console.WriteLine("Extracted Successfully");
+
+            string zipFilePath2 = @"E:\GFDLNFO_BACKADJUSTED_25072024.zip";
+            string extractionPath2 = @"E:\";
+            ZipFile.ExtractToDirectory(zipFilePath2, extractionPath2);
+            Console.WriteLine("Extracted Successfully");
+
+            File.Delete("E:\\BhavCopy_NSE_FO_0_0_0_20240725_F_0000.csv");
+            File.Delete("E:\\GFDLNFO_BACKADJUSTED_25072024.zip");
+            File.Delete("E:\\GFDLNFO_CONTRACT_25072024.zip");
+
+            File.Move(@"E:\GFDLNFO_BACKADJUSTED_25072024.csv", @"G:\GFDLNFO_BACKADJUSTED_25072024.csv");
+            Console.WriteLine("File Moved");
+
+
+        }
+    }
+}
+
+/*using System.IO.Compression;
 
 DirectoryInfo d = new DirectoryInfo(@"E:\Global Datafeeds\");
 FileInfo[] files = d.GetFiles();
@@ -11,4 +47,4 @@ foreach (var file in files)
     ZipFile.ExtractToDirectory(@"E:\\GFDLNFO_BACKADJUSTED_" + file.Name, @"E:\\", true);
     File.Delete(@"E:\\GFDLNFO_BACKADJUSTED_" + file.Name);
     //File.Copy(@"E:\\GFDLNFO_BACKADJUSTED_"+file.Name, @"E:\\Data\\"+file.Name, true);
-}
+}*/
