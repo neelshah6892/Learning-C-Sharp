0ca7a5f921b67be58838f8524249c7d170f90847
Neel <neelshah6892@gmail.com>
2024-07-30 13:28:28 +0530

    f

--- ExtractZipFromFolder/obj/Debug/net6.0/ExtractZipFromFolder.AssemblyInfo.cs
+++ ExtractZipFromFolder/obj/Debug/net6.0/ExtractZipFromFolder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExtractZipFromFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128dce23c0c5bbb1c906d5f31417b915764958ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6b31bfa6583b70082f93a10713ca89f8888c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExtractZipFromFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExtractZipFromFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
