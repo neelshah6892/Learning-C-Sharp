d16a3766951000c2fb0e6f0478dee75ddf77cafe
Neel <neelshah6892@gmail.com>
2024-08-19 22:40:06 +0530

    Updates

--- SampleMAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/SampleMAUI.AssemblyInfo.cs
+++ SampleMAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/SampleMAUI.AssemblyInfo.cs
@@ -11,10 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.samplemaui")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "SampleMAUI")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleMAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cda2b448da922937db86a06ad12a6058b6e81b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+74c9a8349137deb11318efb40b7922070a5051e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleMAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleMAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
