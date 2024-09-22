293a1bebda74951ce56c3545f9c56ec98b99be75
Neel <neelshah6892@gmail.com>
2024-09-22 18:57:43 +0530

    Updates

---
+++ HttpClientStatus/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+DateTime yesterday = DateTime.Now.AddDays(-1);
+
+using (var client = new WebClient())
+{
+    client.DownloadFile("https://nsearchives.nseindia.com/content/fo/BhavCopy_NSE_FO_0_0_0_20240920_F_0000.csv.zip", "D:/bsefiles/BhavCopy_NSE_FO_0_0_0_20240920_F_0000.csv.zip");
+}
