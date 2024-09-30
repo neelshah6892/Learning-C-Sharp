fb7daa37086fe04363aadbdd39c13ea665d49c44
Neel <neelshah6892@gmail.com>
2024-09-30 14:44:17 +0530

    30 Sep 2024

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = [
+                "", "",
+                ];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
