78736b9315715ca08df49b63ff57931255f4b09b
Neel <neelshah6892@gmail.com>
2024-10-06 11:52:15 +0530

    Updates

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -60,13 +60,96 @@ void OperatorExamples()
 }*/
 
 //Loops Exercises
-//Loop 1
-int counter = 0;
-int count = 5;
+//Loop 1 (executes once)
+//int counter = 1;
+/*int count = 5;
 //nuint count = 5;
 while(count > 0)
 {
     count = count * 3;
-    count = count - 1;
+    count = count * -1;
     Console.WriteLine(counter++ + "->" +count);
+}*/
+
+//Loop 2 (executes 6 times)
+/*int j = 2;
+for (int i = 1; i < 100; i = i * 2)
+{
+    j = j - 1;
+    while (j < 25)
+    {
+        // How many times will
+        // the next statement
+        // be executed?
+        j = j + 5;
+        Console.WriteLine(counter);
+        counter++;
+    }
+}*/
+
+//Loop 3 (executes 3 times)
+/*int p = 2;
+for (int q = 2; q < 32; q = q * 2)
+{
+    while (p < q)
+    {
+        // How many times will
+        // the next statement
+        // be executed?
+        p = p * 2;
+        Console.WriteLine(counter);
+        counter++;
+    }
+    q = p - q;
+}*/
+
+//Loop 4 (executes infinite as i will always be 0)
+/*int i = 0;
+int count = 2;
+while (i == 0)
+{
+    count = count * 3;
+    count = count * -1;
+}*/
+
+//Loop 5 (infinite loop)
+//while (true) { int i = 1; }
+
+/*TryAnIf();
+TrySomeLoops();
+TryAnIfElse();
+static void TryAnIf()
+{
+    int someValue = 4;
+    string name = "Bobbo Jr.";
+    if ((someValue == 3) && (name == "Joe"))
+    {
+        Console.WriteLine("x is 3 and the name is Joe");
+    }
+    Console.WriteLine("this line runs no matter what");
 }
+static void TryAnIfElse()
+{
+    int x = 5;
+    if (x == 10)
+    {
+        Console.WriteLine("x must be 10");
+    }
+    else
+    {
+        Console.WriteLine("x isn’t 10");
+    }
+}
+static void TrySomeLoops()
+{
+    int count = 0;
+    while (count < 10)
+    {
+        count = count + 1;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        count = count - 1;
+    }
+    Console.WriteLine("The answer is " + count);
+}*/
