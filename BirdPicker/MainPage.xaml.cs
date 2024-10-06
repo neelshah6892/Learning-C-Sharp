0c6e8fe559c00684f50b2b169b88e764be1995e1
Neel <neelshah6892@gmail.com>
2024-10-06 21:53:38 +0530

    Updates

---
+++ BirdPicker/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace BirdPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BirdPicker.ItemsSource = new string[] {
+                "Duck",
+                "Pigeon",
+                "Penguin",
+                "Ostrich",
+                "Owl"
+            };
+        }
+
+        private void AddBird_Clicked(object sender, EventArgs e)
+        {
+            Birds.Text = Birds.Text + Environment.NewLine + BirdPicker.SelectedItem;
+        }
+    }
+
+}
---
+++ ExperimentWithControls/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace ExperimentWithControls
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnteredText.Text = e.NewTextValue;
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            StepperValue.Text = e.NewValue.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValue.Text = e.NewValue.ToString();
+        }
+    }
+
+}
