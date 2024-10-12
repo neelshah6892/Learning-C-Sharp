1a30a37c1d2b6e084be10872078fa3492ea8cbdc
Neel <neelshah6892@gmail.com>
2024-10-12 23:34:08 +0530

    Updates

--- SampleDATA/Program.cs
+++ SampleDATA/Program.cs
@@ -8,6 +8,33 @@ var dataPath = Path.GetFullPath(@"housing-prices.csv");
 // Load the data into the data frame
 var dataFrame = DataFrame.LoadCsv(dataPath);
 
-dataFrame.Info();
+// Preview of column datatypes
+Console.WriteLine(dataFrame.Info());
 
-dataFrame.Description();
+// Summary of the data
+Console.WriteLine(dataFrame.Description());
+
+// Transform data
+dataFrame["ComputedPrices"] = dataFrame["HistoricalPrice"].Multiply(2);
+Console.WriteLine(dataFrame["ComputedPrices"]);
+
+// Sort data in groups
+var sortedDataFrame = dataFrame.GroupBy("Size");
+Console.WriteLine(sortedDataFrame.Count());
+
+PrimitiveDataFrameColumn<bool> boolFilter = dataFrame["CurrentPrice"].ElementwiseGreaterThan(200000);
+DataFrame filteredDataFrame = dataFrame.Filter(boolFilter);
+Console.WriteLine(filteredDataFrame);
+
+var ids = new List<Single>() { 1, 2, 3, 4, 5, 6 };
+var bedrooms = new List<Single>() { 1, 2, 3, 2, 3, 1 };
+
+var idColumn = new SingleDataFrameColumn("Id", ids);
+var bedroomColumn = new SingleDataFrameColumn("BedroomNumber", bedrooms);
+var dataFrame2 = new DataFrame(idColumn, bedroomColumn);
+
+// Merge two dataframes
+dataFrame = dataFrame.Merge(dataFrame2, new string[] { "Id" }, new string[] { "Id" });
+
+// Save to csv
+DataFrame.SaveCsv(dataFrame, "result.csv", ',');
--- SampleDATA/obj/Debug/net8.0/SampleDATA.AssemblyInfo.cs
+++ SampleDATA/obj/Debug/net8.0/SampleDATA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleDATA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d927241d85f7115bb45c1d4bacd390df5d744a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4921cbd8bc2204ced60d548ccc0f39f6978189a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleDATA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleDATA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
